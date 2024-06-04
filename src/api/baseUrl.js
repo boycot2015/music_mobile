0afc146e2d8a9a2ad0a9c911698204c032ea491f
boycot2015 <boycot2017@163.com>
2024-06-04 11:04:18 +0800

    Update baseUrl.js

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,2 +1,3 @@
-export default 'http://music-api.boycot.top'
+export default 'https://music-api.boycot.top'
 // export default 'http://119.29.254.62:3000'
+// export default https://boycot-music-api.vercel.app
